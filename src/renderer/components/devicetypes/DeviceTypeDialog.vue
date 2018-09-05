2c2cf97ccc55e135baa4a8f8f81fecb995fde934
Jorge Villaverde <jorge.villaverde@sitewhere.com>
2018-09-05 16:07:31 -0300

    Add validations to DeviceTypeDialog.

--- src/renderer/components/devicetypes/DeviceTypeDialog.vue
+++ src/renderer/components/devicetypes/DeviceTypeDialog.vue
@@ -15,22 +15,44 @@
             <v-container fluid>
               <v-layout row wrap>
                 <v-flex xs12>
+                  <v-text-field required class="mt-1" label="Device Type token"
+                    v-model="typeToken" hide-details prepend-icon="info">
+                  </v-text-field>
+                  <div class="verror">
+                    <span v-if="!$v.typeToken.required && $v.$dirty">Device Type token is required.</span>
+                    <span v-if="!$v.typeToken.validToken && $v.$dirty">Device Type token is not valid.</span>
+                  </div>
+                </v-flex>
+                <v-flex xs12>
                   <v-text-field required class="mt-1" label="Type name"
                     v-model="typeName" prepend-icon="info"></v-text-field>
+                  <div class="verror">
+                    <span v-if="$v.typeName.$invalid && $v.$dirty">Name is required.</span>
+                  </div>
                 </v-flex>
                 <v-flex xs12>
                   <v-text-field class="mt-1" multi-line label="Description"
                     v-model="typeDescription" prepend-icon="subject">
                   </v-text-field>
+                  <div class="verror">
+                    <span v-if="$v.typeDescription.$invalid && $v.$dirty">Description is required.</span>
+                  </div>
                 </v-flex>
                 <v-flex xs12>
                   <v-text-field class="mt-1" label="Image URL"
                     v-model="typeImageUrl" prepend-icon="image">
                   </v-text-field>
+                  <div class="verror">
+                    <span v-if="!$v.typeImageUrl.required && $v.$dirty">Image URL is required.</span>
+                    <span v-if="!$v.typeImageUrl.url && $v.$dirty">URL is not valid.</span>
+                  </div>
                 </v-flex>
                 <v-flex xs12>
                   <v-select required :items="containerPolicies" v-model="typeContainerPolicy"
                     label="Container policy" prepend-icon="developer_board"></v-select>
+                  <div class="verror">
+                    <span v-if="$v.typeContainerPolicy.$invalid && $v.$dirty">Container policy is required.</span>
+                  </div>
                 </v-flex>
               </v-layout>
             </v-container>
@@ -66,16 +88,20 @@
 </template>
 
 <script>
-import Utils from '../common/Utils'
-import BaseDialog from '../common/BaseDialog'
-import MetadataPanel from '../common/MetadataPanel'
-import AssetTypeChooser from '../assettypes/AssetTypeChooser'
+import Utils from "../common/Utils";
+import BaseDialog from "../common/BaseDialog";
+import MetadataPanel from "../common/MetadataPanel";
+import AssetTypeChooser from "../assettypes/AssetTypeChooser";
+import { required, helpers, url } from "vuelidate/lib/validators";
+
+const validToken = helpers.regex('validToken', /^[a-zA-Z0-9-_]+$/)
 
 export default {
 
   data: () => ({
     step: null,
     dialogVisible: false,
+    typeToken: null,
     typeName: null,
     typeDescription: null,
     typeImageUrl: null,
@@ -84,15 +110,35 @@ export default {
     metadata: [],
     containerPolicies: [
       {
-        'text': 'Standalone Device',
-        'value': 'Standalone'
+        "text": "Standalone Device",
+        "value": "Standalone"
       }, {
-        'text': 'Composite Device',
-        'value': 'Composite'
+        "text": "Composite Device",
+        "value": "Composite"
       }
     ],
     error: null
   }),
+
+  validations: {
+    typeToken: {
+      required,
+      validToken
+    },
+    typeName: {
+      required
+    },
+    typeDescription: {
+      required
+    },
+    typeImageUrl: {
+      required,
+      url
+    },
+    typeContainerPolicy: {
+      required
+    }
+  },
 
   components: {
     BaseDialog,
@@ -100,95 +146,106 @@ export default {
     AssetTypeChooser
   },
 
-  props: ['title', 'width', 'createLabel', 'cancelLabel'],
+  props: ["title", "width", "createLabel", "cancelLabel"],
 
   computed: {
     // Indicates if first page fields are filled in.
     firstPageComplete: function () {
-      return (!this.isBlank(this.$data.typeName) &&
-        !this.isBlank(this.$data.typeImageUrl) &&
-        this.$data.typeContainerPolicy)
+      this.$v.$touch();
+      return (!this.$v.typeToken.$invalid) && 
+        (!this.$v.typeName.$invalid) &&
+        (!this.$v.typeDescription.$invalid) &&
+        (!this.$v.typeImageUrl.$invalid) &&
+        (!this.$v.typeContainerPolicy.$invalid);
     }
   },
 
   methods: {
     // Generate payload from UI.
     generatePayload: function () {
-      var payload = {}
-      payload.name = this.$data.typeName
-      payload.description = this.$data.typeDescription
-      payload.imageUrl = this.$data.typeImageUrl
-      payload.containerPolicy = this.$data.typeContainerPolicy
-      payload.metadata = Utils.arrayToMetadata(this.$data.metadata)
-      return payload
+      var payload = {};
+      payload.token = this.$data.typeToken;
+      payload.name = this.$data.typeName;
+      payload.description = this.$data.typeDescription;
+      payload.imageUrl = this.$data.typeImageUrl;
+      payload.containerPolicy = this.$data.typeContainerPolicy;
+      payload.metadata = Utils.arrayToMetadata(this.$data.metadata);
+      return payload;
     },
     // Reset dialog contents.
     reset: function () {
-      this.$data.typeName = null
-      this.$data.typeDescription = null
-      this.$data.typeImageUrl = null
-      this.$data.typeContainerPolicy = null
-      this.$data.metadata = []
-      this.$data.step = 1
+      this.$data.typeToken = null;
+      this.$data.typeName = null;
+      this.$data.typeDescription = null;
+      this.$data.typeImageUrl = null;
+      this.$data.typeContainerPolicy = null;
+      this.$data.metadata = [];
+      this.$data.step = 1;
+      this.$v.$reset();
     },
     // Load dialog from a given payload.
     load: function (payload) {
-      this.reset()
+      this.reset();
 
       if (payload) {
-        this.$data.typeName = payload.name
-        this.$data.typeDescription = payload.description
-        this.$data.typeImageUrl = payload.imageUrl
-        this.$data.typeContainerPolicy = payload.containerPolicy
-        this.$data.metadata = Utils.metadataToArray(payload.metadata)
+        this.$data.typeToken = payload.token;
+        this.$data.typeName = payload.name;
+        this.$data.typeDescription = payload.description;
+        this.$data.typeImageUrl = payload.imageUrl;
+        this.$data.typeContainerPolicy = payload.containerPolicy;
+        this.$data.metadata = Utils.metadataToArray(payload.metadata);
       }
     },
     // Called to open the dialog.
     openDialog: function () {
-      this.$data.dialogVisible = true
+      this.$data.dialogVisible = true;
     },
     // Called to open the dialog.
     closeDialog: function () {
-      this.$data.dialogVisible = false
+      this.$data.dialogVisible = false;
     },
     // Called to show an error message.
     showError: function (error) {
-      this.$data.error = error
+      this.$data.error = error;
     },
     // Called after create button is clicked.
     onCreateClicked: function (e) {
-      var payload = this.generatePayload()
-      this.$emit('payload', payload)
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
+      var payload = this.generatePayload();
+      this.$emit("payload", payload);
     },
     // Called after cancel button is clicked.
     onCancelClicked: function (e) {
-      this.$data.dialogVisible = false
+      this.$data.dialogVisible = false;
     },
     // Called when an asset type is chosen or removed.
     onAssetTypeUpdated: function (assetType) {
       if (assetType) {
-        this.$data.typeAssetTypeToken = assetType.token
+        this.$data.typeAssetTypeToken = assetType.token;
       } else {
-        this.$data.typeAssetTypeToken = null
+        this.$data.typeAssetTypeToken = null;
       }
     },
     // Called when a metadata entry has been deleted.
     onMetadataDeleted: function (name) {
-      var metadata = this.$data.metadata
+      var metadata = this.$data.metadata;
       for (var i = 0; i < metadata.length; i++) {
         if (metadata[i].name === name) {
-          metadata.splice(i, 1)
+          metadata.splice(i, 1);
         }
       }
     },
     // Called when a metadata entry has been added.
     onMetadataAdded: function (entry) {
-      var metadata = this.$data.metadata
-      metadata.push(entry)
+      var metadata = this.$data.metadata;
+      metadata.push(entry);
     },
     // Tests whether a string is blank.
     isBlank: function (str) {
-      return (!str || /^\s*$/.test(str))
+      return (!str || /^\s*$/.test(str));
     }
   }
 }
