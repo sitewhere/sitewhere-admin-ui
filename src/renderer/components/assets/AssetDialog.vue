7b91f63de31e47cba3fe7583f876200547ee7580
Jorge Villaverde <jorge.villaverde@sitewhere.com>
2018-09-08 11:23:18 -0300

    Add branding panel to AssetDialog.

--- src/renderer/components/assets/AssetDialog.vue
+++ src/renderer/components/assets/AssetDialog.vue
@@ -8,6 +8,9 @@
           <v-tabs-item key="details" href="#details">
             Asset Details
           </v-tabs-item>
+          <v-tabs-item key="branding" href="#branding">
+            Branding
+          </v-tabs-item>
           <v-tabs-item key="metadata" href="#metadata">
             Metadata
           </v-tabs-item>
@@ -37,15 +40,6 @@
                       </div>
                     </v-flex>
                     <v-flex xs12>
-                      <v-text-field required class="mt-1" label="Image URL"
-                        v-model="assetImageUrl" prepend-icon="image">
-                      </v-text-field>
-                      <div class="verror">
-                        <span v-if="!$v.assetImageUrl.required && $v.$dirty">Image URL is required.</span>
-                        <span v-if="!$v.assetImageUrl.url && $v.$dirty">URL is not valid.</span>
-                      </div>
-                    </v-flex>
-                    <v-flex xs12>
                       <asset-type-chooser :selectedToken="assetTypeToken"
                         chosenText="Asset is based on the type below:"
                         notChosenText="Choose an asset type from the list below:"
@@ -56,6 +50,13 @@
                 </v-container>
                 </v-card-text>
             </v-card>
+          </v-tabs-content>
+          <v-tabs-content key="branding" id="branding">
+            <branding-panel 
+              ref="branding"
+              @payload="onBrandingChanged"
+              :branding="branding">
+            </branding-panel>
           </v-tabs-content>
           <v-tabs-content key="metadata" id="metadata">
             <metadata-panel :metadata="metadata"
@@ -71,6 +72,7 @@
 import Utils from "../common/Utils";
 import BaseDialog from "../common/BaseDialog";
 import IconSelector from "../common/IconSelector";
+import BrandingPanel from "../common/BrandingPanel";
 import MetadataPanel from "../common/MetadataPanel";
 import AssetTypeChooser from "../assettypes/AssetTypeChooser";
 import { required, helpers, url } from "vuelidate/lib/validators";
@@ -84,8 +86,8 @@ export default {
     dialogVisible: false,
     assetToken: null,
     assetName: null,
-    assetImageUrl: null,
     assetTypeToken: null,
+    branding: {},
     metadata: [],
     error: null
   }),
@@ -97,16 +99,13 @@ export default {
     },
     assetName: {
       required
-    },
-    assetImageUrl: {
-      required,
-      url
     }
   },
 
   components: {
     BaseDialog,
     IconSelector,
+    BrandingPanel,
     MetadataPanel,
     AssetTypeChooser
   },
@@ -123,8 +122,12 @@ export default {
       var payload = {};
       payload.token = this.$data.assetToken;
       payload.name = this.$data.assetName;
-      payload.imageUrl = this.$data.assetImageUrl;
       payload.assetTypeToken = this.$data.assetTypeToken;
+      payload.imageUrl = this.$data.branding.imageUrl;
+      payload.icon = this.$data.branding.icon;
+      payload.backgroundColor = this.$data.branding.backgroundColor;
+      payload.foregroundColor = this.$data.branding.foregroundColor;
+      payload.borderColor = this.$data.branding.borderColor;
       payload.metadata = Utils.arrayToMetadata(this.$data.metadata);
       return payload;
     },
@@ -132,8 +135,13 @@ export default {
     reset: function (e) {
       this.$data.assetToken = null;
       this.$data.assetName = null;
-      this.$data.assetImageUrl = null;
       this.$data.assetTypeToken = null;
+      this.$data.branding = {};
+      this.$data.branding.imageUrl = null;
+      this.$data.branding.icon = null;
+      this.$data.branding.backgroundColor = null;
+      this.$data.branding.foregroundColor = null;
+      this.$data.branding.borderColor = null;
       this.$data.metadata = [];
       this.$data.active = "details";
       this.$v.$reset();
@@ -144,8 +152,13 @@ export default {
       if (payload) {
         this.$data.assetToken = payload.token;
         this.$data.assetName = payload.name;
-        this.$data.assetImageUrl = payload.imageUrl;
         this.$data.assetTypeToken = payload.assetType.token;
+        this.$data.branding = {};
+        this.$data.branding.imageUrl = payload.imageUrl;
+        this.$data.branding.icon = payload.icon;
+        this.$data.branding.backgroundColor = payload.backgroundColor;
+        this.$data.branding.foregroundColor = payload.foregroundColor;
+        this.$data.branding.borderColor = payload.borderColor;
         this.$data.metadata = Utils.metadataToArray(payload.metadata);
       }
     },
@@ -187,6 +200,11 @@ export default {
     onMetadataAdded: function (entry) {
       var metadata = this.$data.metadata;
       metadata.push(entry);
+    },
+
+    // Called when branding changes
+    onBrandingChanged: function (branding) {
+      this.$data.branding = branding;
     }
   }
 }
